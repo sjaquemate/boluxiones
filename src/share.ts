e37aef98f9b385a7771222a49bc55ca2b7ad86e9
Teun Schaeken <you@example.com>
2024-03-17 11:36:15 +0100

    fix share, only copy to clipboard

--- src/share.ts
+++ src/share.ts
@@ -1,10 +1,3 @@
-import { UAParser } from 'ua-parser-js'
-
-const webShareApiDeviceTypes: string[] = ['mobile', 'smarttv', 'wearable']
-const parser = new UAParser()
-const browser = parser.getBrowser()
-const device = parser.getDevice()
-
 const GAME_EPOCH = new Date('03/16/2024')
 
 export type EmojiRepresentation = string[][]
@@ -21,23 +14,8 @@ ${emojiRepresentation.map(row => row.join('')).join('\n')}
   
 #ConexionesArgentinas`
   
-  const shareData = { text: textToShare }
-
-  let shareSuccess = false
-
-  try {
-    if (attemptShare(shareData)) {
-      navigator.share(shareData)
-      shareSuccess = true
-    }
-  } catch (error) {
-    shareSuccess = false
-  }
-
-  if (!shareSuccess) {
     navigator.clipboard.writeText(textToShare)
     handleShareToClipboard()
-  }
 }
 
 export function getPuzzleNumber() {
@@ -45,15 +23,3 @@ export function getPuzzleNumber() {
   const differenceInDays = Math.floor(differenceInSeconds / (1000 * 3600 * 24));
   return differenceInDays
 }
-
-const attemptShare = (shareData: object) => {
-  return (
-    // Deliberately exclude Firefox Mobile, because its Web Share API isn't working correctly
-    browser.name?.toUpperCase().indexOf('FIREFOX') === -1 &&
-    webShareApiDeviceTypes.indexOf(device.type ?? '') !== -1 &&
-    navigator.canShare &&
-    navigator.canShare(shareData) &&
-    navigator.share
-  )
-}
-
